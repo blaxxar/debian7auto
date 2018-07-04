000f7fb22afd5eefe3ca831adc7aec4fecb8a787
blaxxar <gregoriusrahadi12@gmail.com>
2018-07-04 22:46:17 +0700

    Update kuntuldeb.sh

--- kuntuldeb.sh
+++ kuntuldeb.sh
@@ -92,33 +92,6 @@ wget -O /etc/squid3/squid.conf "http://vira.cf/squid3.conf"
 sed -i $MYIP2 /etc/squid3/squid.conf;
 service squid3 restart
 
-
-# install stunnel
-apt-get install stunnel4 -y
-cat > /etc/stunnel/stunnel.conf <<-END
-cert = /etc/stunnel/stunnel.pem
-client = no
-socket = a:SO_REUSEADDR=1
-socket = l:TCP_NODELAY=1
-socket = r:TCP_NODELAY=1
-
-
-[dropbear]
-accept = 443
-connect = 127.0.0.1:3128
-
-END
-
-#membuat sertifikat
-openssl genrsa -out key.pem 2048
-openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
--subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
-cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
-
-#konfigurasi stunnel
-sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
-/etc/init.d/stunnel4 restart
-
 # teks berwarna
 apt-get -y install ruby
 gem install lolcat
@@ -152,14 +125,11 @@ chmod +x about
 # finishing
 cd
 chown -R www-data:www-data /home/vps/public_html
-service nginx start
 service openvpn restart
 service cron restart
 service ssh restart
 service dropbear restart
 service squid3 restart
-service webmin restart
-rm -rf ~/.bash_history && history -c
 echo "unset HISTFILE" >> /etc/profile
 
 # info
